401a6457542e7a087c7b34bbb16be8564b2cbce4
KDUser <128939316+KDUser12@users.noreply.github.com>
2024-10-31 23:27:26 +0100

    Initial project

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+# (c) 2024 YourLabXYZ.
+# Licensed under the MIT License.
+
+""" PIP-Installer
+
+PIP-Installer is a program designed to simplify dependency management for
+Python applications. At program startup, it installs the necessary
+modules, the latest stable version of Python, and other dependencies that
+can be challenging to manage for both developers and users.
+"""
+
+__shortname__   = "PIP-Installer"
+__longname__    = "PIP-Installer : Install Application Dependencies"
+__version__     = "1.0-release"
+
+forge_api_latest_release = "https://api.github.com/repos/YourLabXYZ/PIP-Installer/releases/latest"
